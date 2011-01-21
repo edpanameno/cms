47522e5a9c1c25b4ed2634a9576c24c024bfed1a
Ed Panameno <ed.panameno@gmail.com>
2011-01-20 16:15:52 -0800

    Removed old comments

--- system/application/controllers/projects/trac.php
+++ system/application/controllers/projects/trac.php
@@ -33,15 +33,7 @@ class Trac extends Controller {
 
 	function view_ticket($ticket_id, $project_name) {
 
-		// for nice_timespan
 		$this->load->helper("nice_timespan_helper");
-
-		// When creating a note for a ticket, or changing a ticket
-		// I will need to check to see if something has been posted
-		// to this function, if so then we will have to create a new
-		// note for the ticket
-		$action = $this->input->post("submit");
-		
 
 		$this->load->library("form_validation");
 		$this->form_validation->set_rules('text_description', 'Description', 'required');
@@ -53,7 +45,8 @@ class Trac extends Controller {
 		$data['ticket_notes'] = $this->ticket_model->getTicketNotes($ticket_id);
 		//$data['attachements'] = ;
 
-		// Will be used to fill out the drop down lists for changing the ticket
+		// This model contains the data that can be changed for a ticket
+		// by the user.  This will be used to populate the drop down menus
 		$this->load->model("projects/ticket_type_info_model");
 		$data['ticket_statuses'] = $this->ticket_type_info_model->getStatuses();
 		$data['ticket_priorities'] = $this->ticket_type_info_model->getPriorities();
@@ -102,7 +95,6 @@ class Trac extends Controller {
 		else {
 			$this->load->model("projects/ticket_model");
 			$newly_created_ticket_id = $this->ticket_model->create();
-
 			redirect("projects/$project_id/$project_name/trac/$newly_created_ticket_id", '');
 		}
 	}
@@ -111,9 +103,6 @@ class Trac extends Controller {
 
 		$this->load->model('projects/ticket_model');
 		$this->ticket_model->newNote();
-		
 		redirect("/projects/$project_id/$project_name/trac/$ticket_id", "");
-		// where should we re-direct the user?  Or should they
-		// remain in the view ticket window?
 	}
 }
