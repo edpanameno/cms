a34e75fd3e78da8bb32aff46da259d48fcfb21a1
Ed Panameno <ed.panameno@gmail.com>
2011-01-31 21:29:50 -0800

    Minor changes to these files.

--- wiki/local/config.php
+++ wiki/local/config.php
@@ -146,21 +146,21 @@ $UploadUrlFmt = "http://u9-desktop/~ed/cms/wiki/uploads";
 
 ##  The following lines make additional editing buttons appear in the
 ##  edit page for subheadings, lists, tables, etc.
-# $GUIButtons['h2'] = array(400, '\\n!! ', '\\n', '$[Heading]',
-#                     '$GUIButtonDirUrlFmt/h2.gif"$[Heading]"');
-# $GUIButtons['h3'] = array(402, '\\n!!! ', '\\n', '$[Subheading]',
-#                     '$GUIButtonDirUrlFmt/h3.gif"$[Subheading]"');
-# $GUIButtons['indent'] = array(500, '\\n->', '\\n', '$[Indented text]',
-#                     '$GUIButtonDirUrlFmt/indent.gif"$[Indented text]"');
-# $GUIButtons['outdent'] = array(510, '\\n-<', '\\n', '$[Hanging indent]',
-#                     '$GUIButtonDirUrlFmt/outdent.gif"$[Hanging indent]"');
-# $GUIButtons['ol'] = array(520, '\\n# ', '\\n', '$[Ordered list]',
-#                     '$GUIButtonDirUrlFmt/ol.gif"$[Ordered (numbered) list]"');
-# $GUIButtons['ul'] = array(530, '\\n* ', '\\n', '$[Unordered list]',
-#                     '$GUIButtonDirUrlFmt/ul.gif"$[Unordered (bullet) list]"');
-# $GUIButtons['hr'] = array(540, '\\n----\\n', '', '',
-#                     '$GUIButtonDirUrlFmt/hr.gif"$[Horizontal rule]"');
-# $GUIButtons['table'] = array(600,
-#                       '||border=1 width=80%\\n||!Hdr ||!Hdr ||!Hdr ||\\n||     ||     ||     ||\\n||     ||     ||     ||\\n', '', '', 
-#                     '$GUIButtonDirUrlFmt/table.gif"$[Table]"');
+ $GUIButtons['h2'] = array(400, '\\n!! ', '\\n', '$[Heading]',
+                     '$GUIButtonDirUrlFmt/h2.gif"$[Heading]"');
+ $GUIButtons['h3'] = array(402, '\\n!!! ', '\\n', '$[Subheading]',
+                     '$GUIButtonDirUrlFmt/h3.gif"$[Subheading]"');
+ $GUIButtons['indent'] = array(500, '\\n->', '\\n', '$[Indented text]',
+                     '$GUIButtonDirUrlFmt/indent.gif"$[Indented text]"');
+ $GUIButtons['outdent'] = array(510, '\\n-<', '\\n', '$[Hanging indent]',
+                     '$GUIButtonDirUrlFmt/outdent.gif"$[Hanging indent]"');
+ $GUIButtons['ol'] = array(520, '\\n# ', '\\n', '$[Ordered list]',
+                     '$GUIButtonDirUrlFmt/ol.gif"$[Ordered (numbered) list]"');
+ $GUIButtons['ul'] = array(530, '\\n* ', '\\n', '$[Unordered list]',
+                     '$GUIButtonDirUrlFmt/ul.gif"$[Unordered (bullet) list]"');
+ $GUIButtons['hr'] = array(540, '\\n----\\n', '', '',
+                     '$GUIButtonDirUrlFmt/hr.gif"$[Horizontal rule]"');
+ $GUIButtons['table'] = array(600,
+                       '||border=1 width=80%\\n||!Hdr ||!Hdr ||!Hdr ||\\n||     ||     ||     ||\\n||     ||     ||     ||\\n', '', '', 
+                     '$GUIButtonDirUrlFmt/table.gif"$[Table]"');
 
